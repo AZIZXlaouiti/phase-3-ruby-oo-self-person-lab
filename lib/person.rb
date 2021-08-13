66e1f9431b7713f01f119c02abc79bdd16b3dc1d
AZIZXlaouiti <azizrocke@gmail.com>
2021-08-13 10:15:12 -0400

    completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,65 @@
 # your code goes here
+class Person
+    attr_reader :name ,:happiness , :hygiene
+    attr_accessor :bank_account 
+    
+    def initialize(name)
+        @name ,@bank_account ,@happiness ,@hygiene = name ,25 , 8 ,8 
+       
+    end
+    def clean?
+       return  self.hygiene > 7 
+    end
+    def happy?
+        return self.happiness > 7
+    end
+    def get_paid(amount)
+        self.bank_account += amount
+      "all about the benjamins".
+    end
+    def take_bath
+       self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫".
+    end
+    def work_out
+       self.happiness += 2
+       self.hygiene -= 3
+        "♪ another one bites the dust ♫".
+
+     
+    end
+    def call_friend(friend) 
+        [self,friend].each {|p| p.happiness +=3}
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+    def start_conversation(friend, topic)
+        case topic
+        when 'politics' 
+             [self, friend].each{|p| p.happiness -= 2}
+              "blah blah partisan blah lobbyist"
+        when 'weather'   
+            [self,friend].each {|p| p.happiness += 1}
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+    def happiness=(num)
+        @happiness =  if num > 10
+                        10
+                      elsif num < 0
+                        0
+                      else
+                        num
+                      end
+      end
+    
+      def hygiene=(num)
+        @hygiene =  if num > 10
+                      10
+                    elsif num < 0
+                      0
+                    else
+                      num
+                    end
+     end
